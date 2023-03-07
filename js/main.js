76f921c1390c00405026959dd285876d0736028e
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-07 14:08:25 +0100

    milestone 0 completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,47 @@
 'use strict';
 
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        picture: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        picture: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        picture: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        picture: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        picture: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+console.log({team});
+
+/*
+
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+*/
